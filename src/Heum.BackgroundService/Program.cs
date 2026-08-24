8dbfa891f34b13d99f398e105f57cab20dff6b13
Harun.Suljagic <harun.suljagic@ogr.sakarya.edu.tr>
2026-08-24 11:30:25 +0200

    Fix service discovery for the Azure Service Bus client

--- src/Heum.BackgroundService/Program.cs
+++ src/Heum.BackgroundService/Program.cs
@@ -1,8 +1,21 @@
-using Heum.BackgroundService;
 using Heum.BackgroundService.Outbox;
+using Heum.Contracts.Events;
+using Heum.Data;
+using Heum.Data.Auditing;
+using Heum.Infrastructure.Messaging;
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+
+builder.Services.AddSingleton(TimeProvider.System);
+builder.Services.AddScoped<ICurrentUserService, SystemCurrentUserService>();
+
+builder.AddDatabase();
+
+builder.AddAzureServiceBusClient("messaging");
+builder.AddEventPublishing(topics => topics
+    .MapTopic<TenantCreatedEvent>("tenant-events")
+    .MapTopic<UserOnboardingRequestedEvent>("user-events")
+    .MapTopic<InvitationCreatedEvent>("user-events"));
 
 builder.Services.Configure<OutboxProcessorOptions>(
     builder.Configuration.GetSection(OutboxProcessorOptions.SectionName));
