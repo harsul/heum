fe0d0d0c2c2eea3e47c0d0880ba5ab9ba36a9f4d
Harun.Suljagic <harun.suljagic@ogr.sakarya.edu.tr>
2026-08-26 21:42:12 +0200

    Mov configuration to the model folder

--- src/Heum.Data/Models/Configurations/TenantConfiguration.cs
+++ src/Heum.Data/Models/Configurations/TenantConfiguration.cs
@@ -1,8 +1,7 @@
-using Heum.Data.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace Heum.Data.Configurations;
+namespace Heum.Data.Models.Configurations;
 
 public class TenantConfiguration : IEntityTypeConfiguration<Tenant>
 {
