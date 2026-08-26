744a59227991049966dc75a6f69580cd6d08ad44
Harun.Suljagic <harun.suljagic@ogr.sakarya.edu.tr>
2026-08-26 17:04:23 +0200

    Revert "Send webfrontend as reference to the function and configure onboarding"
    This reverts commit 07e482a41aff24f433a093f49698dfa658103267.

--- src/Heum.AppHost/AppHost.cs
+++ src/Heum.AppHost/AppHost.cs
@@ -69,11 +69,10 @@ builder.AddAzureFunctionsProject<Projects.Heum_Functions>("useronboarding")
     .WithReference(cache)
     .WithReference(keycloak)
     .WithReference(messaging)
-    .WithReference(webfrontend)
     .WaitFor(cache)
     .WaitFor(keycloak)
     .WaitFor(messaging)
-    .WaitFor(webfrontend)
-    .WithEnvironment("KeycloakAdmin__ClientSecret", keycloakAdminSecret);
+    .WithEnvironment("KeycloakAdmin__ClientSecret", keycloakAdminSecret)
+    .WithEnvironment("KeycloakAdmin__OnboardingRedirectUri", webfrontend.GetEndpoint("http").Property(EndpointProperty.Url));
 
 builder.Build().Run();
--- src/Heum.Functions/Program.cs
+++ src/Heum.Functions/Program.cs
@@ -1,7 +1,6 @@
 ﻿using Heum.Infrastructure.Keycloak;
 using Heum.ServiceDefaults;
 using Microsoft.Azure.Functions.Worker.Builder;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var builder = FunctionsApplication.CreateBuilder(args);
@@ -15,16 +14,5 @@ builder.AddRedisClientBuilder("cache")
     .WithDistributedCache();
 
 builder.AddKeycloakAdmin();
-
-// WithReference(webfrontend) in AppHost injects the URL via service discovery env vars.
-// Map it to the options property that KeycloakAdminClient uses when sending action emails.
-builder.Services.PostConfigure<KeycloakAdminOptions>(options =>
-{
-    if (string.IsNullOrEmpty(options.OnboardingRedirectUri))
-        options.OnboardingRedirectUri =
-            builder.Configuration["services__webfrontend__https__0"] ??
-            builder.Configuration["services__webfrontend__http__0"] ??
-            string.Empty;
-});
 
 builder.Build().Run();
